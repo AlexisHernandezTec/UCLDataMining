4b7d033887c3676e78604e9e905180da496d9683
AlexHP <101281329+K1ll1NGSN1PER@users.noreply.github.com>
2024-09-18 21:53:41 -0600

    feat(results): transform the dataframe, add new column with the year and just filter with the 2010 or higher season

--- results.py
+++ results.py
@@ -1,1 +1,6 @@
 import pandas as pd
+df = pd.read_csv('datasets/ucl-finals.csv')
+df['year'] = df['season'].str[:2] + df['season'].str[-2:]
+df['year'] = df['year'].astype(int)
+df = df[df['year'] >= 2010]
+df
